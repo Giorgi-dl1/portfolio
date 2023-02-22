a7542d154ed89e411ab7f51acdf3defe508b0555
Giorgi <drgls@mail.ru>
2023-02-22 20:45:03 +0400

    implement home section

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react'
+import logo from '../images/logo.png'
+
+const Navbar = () => {
+  const threshold = 20
+  const [showNav, setShowNav] = useState(true)
+
+  useEffect(() => {
+    let previousScrollYPosition = window.scrollY
+
+    const scrolledMoreThanThreshold = (currentScrollYPosition: number) =>
+      Math.abs(currentScrollYPosition - previousScrollYPosition) > threshold
+
+    const isScrollingUp = (currentScrollYPosition: number) =>
+      currentScrollYPosition > previousScrollYPosition &&
+      !(previousScrollYPosition > 0 && currentScrollYPosition === 0) &&
+      !(currentScrollYPosition > 0 && previousScrollYPosition === 0)
+
+    const updateScrollDirection = () => {
+      const currentScrollYPosition = window.scrollY
+
+      if (scrolledMoreThanThreshold(currentScrollYPosition)) {
+        const showNav = isScrollingUp(currentScrollYPosition) ? false : true
+        setShowNav(showNav)
+        previousScrollYPosition =
+          currentScrollYPosition > 0 ? currentScrollYPosition : 0
+      }
+    }
+
+    const onScroll = () => window.requestAnimationFrame(updateScrollDirection)
+
+    window.addEventListener('scroll', onScroll)
+
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [])
+  const linkNames = [
+    { name: 'Home', color: '#6D2ED5' },
+    { name: 'Skills', color: '#D14C32' },
+    { name: 'Work', color: '#2D68F0' },
+  ]
+
+  return (
+    <nav
+      className={`flex z-50 items-center transition-all duration-300 ease-in right-0 fixed left-0 -top-[84px] ${
+        showNav && 'shadow-md !top-0'
+      } bg-white justify-between padding-x`}
+    >
+      <img src={logo} draggable={false} className="h-[70px]" alt="" />
+      <ul className="flex gap-3 font-semibold">
+        {linkNames.map((link) => (
+          <a
+            className={`text-black overflow-hidden group/link transition duration-300 hover:!text-[${link.color}]`}
+            key={link.name}
+            href={`#${link.name.toLocaleLowerCase()}`}
+          >
+            <li>{link.name}</li>
+            <div
+              className={`w-full -translate-x-[110%] transition duration-300 group-hover/link:translate-x-0 h-0.5 bg-[${link.color}]`}
+            />
+          </a>
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,5 +1,65 @@
+import { TypeAnimation } from 'react-type-animation'
+import Navbar from '../components/Navbar'
+import { FiDownload } from 'react-icons/fi'
+import { BsLinkedin, BsGithub } from 'react-icons/bs'
+import { useEffect, useState } from 'react'
 const Home = () => {
-  return <div className="h-screen">Home</div>
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    if (!mounted) {
+      setMounted(true)
+    }
+  }, [])
+  return (
+    <div id="home" className="flex items-center h-screen padding-x">
+      <Navbar />
+      <div className="space-y-3 name pt-[70px]">
+        <h1
+          className={`translate-y-[100px] opacity-0 transition duration-1000 ${
+            mounted && '!translate-y-0 !opacity-100'
+          } text-2xl font-bold text-blue-900 md:text-6xl`}
+        >
+          Hi There,
+          <br />
+          I'm <span className="text-orange-500">Giorgi Durglishvili</span>
+        </h1>
+        <p
+          className={`-translate-x-[200px] opacity-0 transition delay-[800ms] duration-700 ${
+            mounted && '!translate-x-0 !opacity-100'
+          } flex gap-1 text-sm font-bold md:text-xl`}
+        >
+          <span>I Am Into</span>
+          <TypeAnimation
+            sequence={[' Frontend Development', 2000, '']}
+            wrapper="span"
+            cursor={true}
+            repeat={Infinity}
+            className="text-red-700 "
+          />
+        </p>
+        <button className="flex items-center gap-1 px-3 py-1 font-bold text-white transition bg-blue-900 rounded-md hover:bg-blue-700 duratio-300 md:px-6 md:py-3">
+          Download CV
+          <FiDownload />
+        </button>
+        <div className="flex gap-2">
+          <a
+            href="https://www.linkedin.com/in/giorgi-durglishvili-92ba2425a/"
+            target="_blank"
+          >
+            <div className="icon hover:bg-[#0077b5]">
+              <BsLinkedin />
+            </div>
+          </a>
+          <a href="https://github.com/Giorgi-dl1" target="_blank">
+            <div className="icon hover:bg-black">
+              <BsGithub />
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
