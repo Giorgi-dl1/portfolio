b498d518d2829676fd465d59b9acda08797a15fe
Giorgi <drgls@mail.ru>
2023-02-24 02:40:13 +0400

    update app render method

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 import './dist/output.css'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 )
 
 serviceWorker.unregister()
