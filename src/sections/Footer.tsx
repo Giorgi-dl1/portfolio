319e6a7cb284ee0994fe4976ae072e9024b44a99
Giorgi <drgls@mail.ru>
2023-02-24 01:43:56 +0400

    implement footer section

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,5 +1,37 @@
+import { AiFillPhone } from 'react-icons/ai'
+import { GrMail } from 'react-icons/gr'
+import { MdLocationPin } from 'react-icons/md'
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div className="bg-[#001244] padding-x py-6 text-white">
+      <div className="max-w-[1200px] mx-auto flex justify-between flex-wrap gap-6">
+        <div className="max-w-[340px] space-y-3">
+          <h3 className="text-2xl font-semibold">Giorgi's Portfolio</h3>
+          <p className="text-sm">
+            Thank you for visiting my personal portfolio website. Connect with
+            me over socials or live chat.
+          </p>
+        </div>
+        <div className="max-w-[340px] space-y-3">
+          <h3 className="text-2xl font-semibold">Contact Info</h3>
+          <div className="space-y-3">
+            <div className="flex items-center gap-1">
+              <AiFillPhone className="text-orange-500" />
+              <span>+995 598 42 15 47</span>
+            </div>
+            <div className="flex items-center gap-1">
+              <GrMail className="text-orange-500" />
+              <span>gdurglishvili.dl@gmail.com</span>
+            </div>
+            <div className="flex items-center gap-1">
+              <MdLocationPin className="text-orange-500" />
+              <span>Tbilisi, Georgia</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
