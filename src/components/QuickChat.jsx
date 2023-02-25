7496d5020c39beb681b361dd3288d8bdc1789c0b
Giorgi <drgls@mail.ru>
2023-02-25 15:19:12 +0400

    fix hovering issue

--- src/components/QuickChat.jsx
+++ src/components/QuickChat.jsx
@@ -5,21 +5,22 @@ import ContactForm from './ContactForm'
 const QuickChat = () => {
   const [showForm, setShowForm] = useState(false)
   return (
-    <div className="fixed z-[100] bottom-6 left-4">
+    <>
       <div
         className={`p-3 pointer-events-none bg-white border scale-75 opacity-0 transition-all duration-300 border-[#ccc] rounded shadow-2xl ${
-          showForm && 'scale-100 pointer-events-auto opacity-100'
+          showForm &&
+          '!scale-100 pointer-events-auto !opacity-100 fixed z-[100] bottom-20 left-4'
         }`}
       >
         <ContactForm sm={true} />
       </div>
       <div
         onClick={() => setShowForm(!showForm)}
-        className="p-4 mt-3 text-xl text-white bg-red-700 rounded-full cursor-pointer max-w-max"
+        className="p-4 fixed z-[100] bottom-6 left-4 mt-3 text-xl text-white bg-red-700 rounded-full cursor-pointer max-w-max"
       >
         {showForm ? <AiOutlineClose /> : <TbMessageCircle />}
       </div>
-    </div>
+    </>
   )
 }
 
