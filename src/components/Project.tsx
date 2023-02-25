2a0970edb2794477783a93e7852c4e114db56968
Giorgi <drgls@mail.ru>
2023-02-25 15:21:17 +0400

    add more projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,5 +1,6 @@
 import { BsFillPlayFill, BsGithub } from 'react-icons/bs'
 import { project } from '../interfaces'
+import { motion as m } from 'framer-motion'
 interface ProjectComponent {
   project: project
   index: number
@@ -7,7 +8,13 @@ interface ProjectComponent {
 
 const Project = ({ project, index }: ProjectComponent) => {
   return (
-    <div className="max-w-[300px] md:max-w-[520px] relative group/card overflow-hidden w-full h-[150px] md:h-[270px] rounded-xl p-[8px] md:p-[12px] border border-[#fafafa62]">
+    <m.div
+      initial={{ opacity: 0, scale: 0.8 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0.8 }}
+      transition={{ duration: 0.6 }}
+      className="max-w-[300px] md:max-w-[520px] relative group/card overflow-hidden w-full h-[150px] md:h-[270px] rounded-xl p-[8px] md:p-[12px] border border-[#fafafa62]"
+    >
       <div className="relative  rounded-xl overflow-hidden transition-all duration-500 w-[85%] group-hover/card:w-full h-[130px] md:h-[246px]">
         <div className="absolute z-[2] top-0 left-0 w-full h-full bg-[#33333334]" />
         <img
@@ -61,7 +68,7 @@ const Project = ({ project, index }: ProjectComponent) => {
           (index + 1) % 2 ? 'bg-blue-900' : 'bg-red-700'
         }  rounded-full group-hover/card:opacity-50 right-3 md:right-6 bottom-4 md:bottom-8 group-hover/card:scale-[19] md:group-hover/card:scale-[18] transition-all duration-500`}
       />
-    </div>
+    </m.div>
   )
 }
 
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,30 +1,49 @@
 import Project from '../components/Project'
 import data from '../data.json'
+import { AiOutlineArrowRight } from 'react-icons/ai'
 import { project } from '../interfaces'
+import { useState } from 'react'
+import { AnimatePresence } from 'framer-motion'
 
 const Projects = () => {
+  const [showMore, setShowMore] = useState(false)
+
   const { projects }: { projects: project[] } = data
+  const getProjects = () => {
+    return showMore ? [...projects] : projects.slice(0, 4)
+  }
+
   return (
-    <div
-      id="work"
-      className="relative grid min-h-screen padding-x place-content-center"
-    >
-      <img
-        src="https://i.pinimg.com/originals/98/33/5f/98335fcd764b32cae3780b1e1d5d39b4.gif"
-        className="absolute z-[0] top-0 left-0 object-cover w-full h-full"
-        alt=""
-      />
-      <div className="relative z-[1] py-8 space-y-8">
-        <h2 className="text-4xl font-bold text-center text-white name ">
-          Recent <span className="text-orange-500">Projects</span>
-        </h2>
-        <div className="flex max-w-[1200px] md:mx-auto flex-wrap justify-center gap-y-7 gap-x-4 lg:justify-between">
-          {projects.map((project, index) => (
-            <Project index={index} project={project} key={project.name} />
-          ))}
+    <AnimatePresence initial={false} mode="wait">
+      <div
+        id="work"
+        className="relative grid min-h-screen padding-x place-content-center"
+      >
+        <img
+          src="https://i.pinimg.com/originals/98/33/5f/98335fcd764b32cae3780b1e1d5d39b4.gif"
+          className="absolute z-[0] top-0 left-0 object-cover w-full h-full"
+          alt=""
+        />
+        <div className="relative z-[1] py-8 space-y-8">
+          <h2 className="text-4xl font-bold text-center text-white name ">
+            Recent <span className="text-orange-500">Projects</span>
+          </h2>
+          <div
+            className={`flex max-w-[1200px]  transition-all duration-500 md:mx-auto flex-wrap justify-center gap-y-7 gap-x-4 lg:justify-between`}
+          >
+            {getProjects().map((project, index) => (
+              <Project index={index} project={project} key={project.name} />
+            ))}
+          </div>
+        </div>
+        <div
+          onClick={() => setShowMore(!showMore)}
+          className="relative flex items-center gap-1 px-6 py-2 mx-auto mb-4 font-bold text-white border border-white rounded shadow-sm cursor-pointer shadow-white hover:bg-opacity-90 max-w-max"
+        >
+          <span>View More</span> <AiOutlineArrowRight />
         </div>
       </div>
-    </div>
+    </AnimatePresence>
   )
 }
 
